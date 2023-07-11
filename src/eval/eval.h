51c17cfac8266c6c8b807e92e4c2e12faa28d831
Ciekce <kantaja98@gmail.com>
2023-07-11 05:57:50 +0100

    384 hidden neurons

--- src/eval/eval.h
+++ src/eval/eval.h
@@ -51,7 +51,7 @@ namespace stormphrax::eval
 	using Activation = activation::ClippedReLU<0, 255>;
 
 	constexpr u32 InputSize = 768;
-	constexpr u32 Layer1Size = 64;
+	constexpr u32 Layer1Size = 384;
 
 	constexpr Score Scale = 400;
 
