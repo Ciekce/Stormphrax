Ciekce/Stormphrax

commit cd8c74f116dd02755bde5031fb87f774187b56d9

allow non-power of 2 ttable sizes
Bench: 12364879

src/ttable.cpp
@@ -55,27 +55,19 @@ namespace stormphrax
 
 	auto TTable::resize(usize size) -> void
 	{
-		clear();
-
 		size *= 1024 * 1024;
 
-		const usize capacity = std::bit_floor(size / sizeof(TTableEntry));
+		const auto capacity = size / sizeof(TTableEntry);
 
 		//TODO handle oom
 		m_table.resize(capacity);
 		m_table.shrink_to_fit();
 
-		if (capacity > 0)
-			std::memset(m_table.data(), 0, capacity * sizeof(TTableEntry));
-
-		m_mask = capacity - 1;
+		clear();
 	}
 
 	auto TTable::probe(ProbedTTableEntry &dst, u64 key, i32 depth, i32 ply, Score alpha, Score beta) const -> bool
 	{
-		if (m_table.empty())
-			return false;
-
 		const auto entry = loadEntry(key);
 
 		if (entry.type != EntryType::None
@@ -111,9 +103,6 @@ namespace stormphrax
 
 	auto TTable::probePvMove(u64 key) const -> Move
 	{
-		if (m_table.empty())
-			return NullMove;
-
 		const auto entry = loadEntry(key);
 
 		if (entry.type == EntryType::Exact
@@ -125,9 +114,6 @@ namespace stormphrax
 
 	auto TTable::put(u64 key, Score score, Move move, i32 depth, i32 ply, EntryType type) -> void
 	{
-		if (m_table.empty())
-			return;
-
 		auto entry = loadEntry(key);
 
 		const auto entryKey = static_cast<u16>(key >> 48);
@@ -168,8 +154,7 @@ namespace stormphrax
 		m_entries = 0;
 		m_currentAge = 0;
 
-		if (!m_table.empty())
-			std::memset(m_table.data(), 0, m_table.size() * sizeof(TTableEntry));
+		std::memset(m_table.data(), 0, m_table.size() * sizeof(TTableEntry));
 	}
 
 	auto TTable::full() const -> u32

src/ttable.h
@@ -83,7 +83,7 @@ namespace stormphrax
 			if (m_table.empty())
 				return;
 
-			__builtin_prefetch(&m_table[key & m_mask]);
+			__builtin_prefetch(&m_table[index(key)]);
 		}
 
 		inline auto age()
@@ -92,9 +92,15 @@ namespace stormphrax
 		}
 
 	private:
+		[[nodiscard]] inline auto index(u64 key) const -> u64
+		{
+			// this emits a single mul on both x64 and arm64
+			return static_cast<u64>((static_cast<u128>(key) * static_cast<u128>(m_table.size())) >> 64);
+		}
+
 		[[nodiscard]] inline auto loadEntry(u64 key) const
 		{
-			const auto *ptr = static_cast<volatile const i64 *>(&m_table[key & m_mask]);
+			const auto *ptr = static_cast<volatile const i64 *>(&m_table[index(key)]);
 			const auto v = *ptr;
 
 			TTableEntry entry{};
@@ -108,13 +114,12 @@ namespace stormphrax
 			i64 v{};
 			std::memcpy(&v, &entry, sizeof(TTableEntry));
 
-			auto *ptr = static_cast<volatile i64 *>(&m_table[key & m_mask]);
+			auto *ptr = static_cast<volatile i64 *>(&m_table[index(key)]);
 			__atomic_exchange(ptr, &v, &v, __ATOMIC_ACQUIRE);
 
 			std::memcpy(&entry, &v, sizeof(TTableEntry));
 		}
 
-		u64 m_mask{};
 		std::vector<i64> m_table{};
 
 		std::atomic_size_t m_entries{};

src/types.h
@@ -27,11 +27,13 @@ namespace stormphrax
 	using u16 = std::uint16_t;
 	using u32 = std::uint32_t;
 	using u64 = std::uint64_t;
+	using u128 = unsigned __int128;
 
 	using i8 = std::int8_t;
 	using i16 = std::int16_t;
 	using i32 = std::int32_t;
 	using i64 = std::int64_t;
+	using i128 = __int128;
 
 	using f32 = float;
 	using f64 = double;

commit 7246fe4f507da8ec2474540a5c8aa6cc48a6ec58

resolve 400 elo regression from cd8c74f
Bench: 11169702

src/ttable.cpp
@@ -46,6 +46,11 @@ namespace stormphrax
 				return score + ply;
 			return score;
 		}
+
+		inline u16 packEntryKey(u64 key)
+		{
+			return static_cast<u16>(key);
+		}
 	}
 
 	TTable::TTable(usize size)
@@ -71,7 +76,7 @@ namespace stormphrax
 		const auto entry = loadEntry(key);
 
 		if (entry.type != EntryType::None
-			&& static_cast<u16>(key >> 48) == entry.key)
+			&& packEntryKey(key) == entry.key)
 		{
 			dst.score = scoreFromTt(static_cast<Score>(entry.score), ply);
 			dst.depth = entry.depth;
@@ -106,7 +111,7 @@ namespace stormphrax
 		const auto entry = loadEntry(key);
 
 		if (entry.type == EntryType::Exact
-		    && static_cast<u16>(key >> 48) == entry.key)
+		    && packEntryKey(key) == entry.key)
 			return entry.move;
 
 		return NullMove;
@@ -116,7 +121,7 @@ namespace stormphrax
 	{
 		auto entry = loadEntry(key);
 
-		const auto entryKey = static_cast<u16>(key >> 48);
+		const auto entryKey = packEntryKey(key);
 
 		// always replace empty entries
 		const bool replace = entry.key == 0