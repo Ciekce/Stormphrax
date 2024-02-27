a8cb64875a79417310c3718b6950623142006acb
Ciekce <kantaja98@gmail.com>
2024-02-27 05:25:41 +0000

    update copyright
    Bench: 4399604

--- src/limit/trivial.h
+++ src/limit/trivial.h
@@ -1,6 +1,6 @@
 /*
  * Stormphrax, a UCI chess engine
- * Copyright (C) 2023 Ciekce
+ * Copyright (C) 2024 Ciekce
  *
  * Stormphrax is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
