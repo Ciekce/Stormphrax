0d25b784bf338525deaab3f4847606f382c7cf2f
Ciekce <kantaja98@gmail.com>
2024-03-11 13:08:35 +0000

    increase default bench depth
    Bench: 5605304

--- src/bench.h
+++ src/bench.h
@@ -27,7 +27,7 @@ namespace stormphrax::bench
 #ifdef SP_PGO_PROFILE
 	constexpr i32 DefaultBenchDepth = 12;
 #else
-	constexpr i32 DefaultBenchDepth = 14;
+	constexpr i32 DefaultBenchDepth = 15;
 #endif
 
 	auto run(search::Searcher &searcher, i32 depth = DefaultBenchDepth) -> void;
