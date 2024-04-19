fecec4b0d1f726312b4deff0a6965f2335c457d0
Ciekce <kantaja98@gmail.com>
2024-04-19 09:01:53 +0100

    increase bench depth
    Bench: 5895962

--- src/bench.h
+++ src/bench.h
@@ -27,7 +27,7 @@ namespace stormphrax::bench
 #ifdef SP_PGO_PROFILE
 	constexpr i32 DefaultBenchDepth = 3;
 #else
-	constexpr i32 DefaultBenchDepth = 9;
+	constexpr i32 DefaultBenchDepth = 10;
 #endif
 
 	constexpr usize DefaultBenchTtSize = 16;
