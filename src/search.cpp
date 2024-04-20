aa70cd8f25de794a82a398fdcfd6d2cc22905b8d
Ciekce <kantaja98@gmail.com>
2024-04-20 01:15:50 +0100

    tunability again
    Bench: 4440234

--- src/search.cpp
+++ src/search.cpp
@@ -479,7 +479,8 @@ namespace stormphrax::search
 				&& !thread.stack[ply - 1].move.isNull()
 				&& !bbs.nonPk(us).empty())
 			{
-				const auto R = nmpBaseReduction() + depth / 4;
+				const auto R = nmpBaseReduction()
+					+ depth / nmpDepthReductionDiv();
 
 				stack.move = NullMove;
 				const auto guard = pos.applyNullMove();
--- src/tunable.h
+++ src/tunable.h
@@ -97,6 +97,7 @@ namespace stormphrax::tunable
 
 	SP_TUNABLE_PARAM(minNmpDepth, 4, 3, 8, 0.5)
 	SP_TUNABLE_PARAM(nmpBaseReduction, 4, 2, 5, 0.5)
+	SP_TUNABLE_PARAM(nmpDepthReductionDiv, 4, 1, 8, 1)
 
 	SP_TUNABLE_PARAM(minLmrDepth, 2, 2, 5, 1)
 	SP_TUNABLE_PARAM(lmrMinMoves, 3, 0, 5, 1)
