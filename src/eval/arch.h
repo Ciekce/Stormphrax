2e31c296f2e8a9b6cade71e52d06398660b5f163
Ciekce <kantaja98@gmail.com>
2024-06-30 22:53:40 +0100

    viri gestalt net arch

--- src/eval/arch.h
+++ src/eval/arch.h
@@ -41,15 +41,15 @@ namespace stormphrax::eval
 
 	// visually flipped upside down, a1 = 0
 	using InputFeatureSet = nnue::features::KingBucketsMirrored<
-		0, 0, 1, 1,
-		2, 2, 2, 2,
-		3, 3, 3, 3,
-		3, 3, 3, 3,
-		3, 3, 3, 3,
-		3, 3, 3, 3,
-		3, 3, 3, 3,
-		3, 3, 3, 3
+		0, 1, 2, 3,
+		4, 4, 5, 5,
+		6, 6, 6, 6,
+		7, 7, 7, 7,
+		8, 8, 8, 8,
+		8, 8, 8, 8,
+		8, 8, 8, 8,
+		8, 8, 8, 8
 	>;
 
-	using OutputBucketing = nnue::output::MaterialCount<8>;
+	using OutputBucketing = nnue::output::Single;
 }
