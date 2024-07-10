b241fdd6c4914ab9ca6a647965508e036dd6495e
Ciekce <kantaja98@gmail.com>
2024-07-10 06:03:49 +0100

    lmr more if static eval is at least beta
    Bench: 4443729

--- src/search.cpp
+++ src/search.cpp
@@ -860,6 +860,7 @@ namespace stormphrax::search
 					r -= history / lmrHistoryDivisor();
 					r -= improving;
 					r -= pos.isCheck();
+					r += !inCheck && std::abs(alpha) < ScoreWin && curr.staticEval >= beta;
 
 					// can't use std::clamp because newDepth can be <0
 					const auto reduced = std::min(std::max(newDepth - r, 1), newDepth);
