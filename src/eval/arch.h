f120446b6571867d60c78c36786d02f039a5b172
Ciekce <kantaja98@gmail.com>
2024-07-18 09:23:43 +0100

    more buckets, smaller l1

--- src/eval/arch.h
+++ src/eval/arch.h
@@ -35,20 +35,20 @@ namespace stormphrax::eval
 
 	using L1Activation = nnue::activation::SquaredClippedReLU<i16, i32, L1Q>;
 
-	constexpr u32 L1Size = 1536;
+	constexpr u32 L1Size = 1280;
 
 	constexpr i32 Scale = 400;
 
 	// visually flipped upside down, a1 = 0
 	using InputFeatureSet = nnue::features::KingBucketsMirrored<
-		0, 0, 1, 1,
-		2, 2, 2, 2,
-		3, 3, 3, 3,
-		3, 3, 3, 3,
-		3, 3, 3, 3,
-		3, 3, 3, 3,
-		3, 3, 3, 3,
-		3, 3, 3, 3
+		 0,  1,  2,  3,
+		 4,  5,  6,  7,
+		 8,  9, 10, 11,
+		 8,  9, 10, 11,
+		12, 12, 13, 13,
+		12, 12, 13, 13,
+		14, 14, 15, 15,
+		14, 14, 15, 15
 	>;
 
 	using OutputBucketing = nnue::output::MaterialCount<8>;
