e532c5b75cd99f4046741555a471844cfb1db91c
Ciekce <kantaja98@gmail.com>
2024-08-02 00:45:34 +0100

    shrink HL

--- src/eval/arch.h
+++ src/eval/arch.h
@@ -35,7 +35,7 @@ namespace stormphrax::eval
 
 	using L1Activation = nnue::activation::SquaredClippedReLU<i16, i32, L1Q>;
 
-	constexpr u32 L1Size = 1280;
+	constexpr u32 L1Size = 128;
 
 	constexpr i32 Scale = 400;
 
