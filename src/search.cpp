015afe324dc94541e5bb0480d5dabe04511a6c7c
Ciekce <kantaja98@gmail.com>
2024-11-09 20:32:53 +0000

    eval in check!
    Bench: 4276739

--- src/search.cpp
+++ src/search.cpp
@@ -828,7 +828,7 @@ namespace stormphrax::search
 
 					const auto gain = see::gain(boards, move);
 
-					if (depth <= 5 && curr.staticEval + 60 * depth + gain * 36 / 16 <= alpha)
+					if (!inCheck && depth <= 5 && curr.staticEval + 60 * depth + gain * 36 / 16 <= alpha)
 						continue;
 				}
 
