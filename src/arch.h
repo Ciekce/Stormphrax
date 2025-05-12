bc4d2af804686e7bcdd720f58a71ef6b3d8a3877
Ciekce <44617491+Ciekce@users.noreply.github.com>
2025-05-12 07:12:51 +0100

    disable VNNI for native builds
    actually a slowdown
    
    Bench: 4322334

--- src/arch.h
+++ src/arch.h
@@ -29,7 +29,7 @@
 	#else
 		#define SP_HAS_BMI2 0
 	#endif
-	#define SP_HAS_AVX512VNNI __AVX512VNNI__
+	#define SP_HAS_AVX512VNNI 0
 	#define SP_HAS_AVX512 (__AVX512F__ && (__AVX512BW__ || __AVX512VNNI__))
 	#define SP_HAS_AVX2 __AVX2__
 	#define SP_HAS_POPCNT __POPCNT__
