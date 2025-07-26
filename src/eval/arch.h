86775b26cf5147dfa3a6de4a39bc4979c7aeeca1
Ciekce <44617491+Ciekce@users.noreply.github.com>
2025-07-27 00:26:09 +0100

    SP 2 arch
    Bench: 4059472

--- src/eval/arch.h
+++ src/eval/arch.h
@@ -35,24 +35,14 @@ namespace stormphrax::eval
 
 	constexpr bool PairwiseMul = false;
 
-	constexpr u32 L1Size = 1536;
+	constexpr u32 L1Size = 384;
 
-	using L1Activation = nnue::activation::SquaredClippedReLU<i16, i32, L1Q>;
+	using L1Activation = nnue::activation::ClippedReLU<i16, i32, L1Q>;
 
 	constexpr i32 Scale = 400;
 
 	// visually flipped upside down, a1 = 0
-	using InputFeatureSet = nnue::features::KingBucketsMirrored<
-		nnue::features::MirroredKingSide::Abcd,
-		 0,  1,  2,  3,
-		 4,  5,  6,  7,
-		 8,  9, 10, 11,
-		 8,  9, 10, 11,
-		12, 12, 13, 13,
-		12, 12, 13, 13,
-		14, 14, 15, 15,
-		14, 14, 15, 15
-	>;
+	using InputFeatureSet = nnue::features::SingleBucket;
 
-	using OutputBucketing = nnue::output::MaterialCount<8>;
+	using OutputBucketing = nnue::output::Single;
 }
