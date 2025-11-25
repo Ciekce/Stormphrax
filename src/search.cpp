c9445dabc064cd30d3f2d11a2597fdc8c5f93ebe
Ciekce <44617491+Ciekce@users.noreply.github.com>
2025-11-25 20:48:28 +0000

    tunability + version
    Bench: 6628611

--- src/search.cpp
+++ src/search.cpp
@@ -903,7 +903,7 @@ namespace stormphrax::search {
                     }
 
                     if (!inCheck && lmrDepth <= 8 && std::abs(alpha) < 2000
-                        && curr.staticEval + fpMargin() + depth * fpScale() + history / 128 <= alpha)
+                        && curr.staticEval + fpMargin() + depth * fpScale() + history / fpHistoryDivisor() <= alpha)
                     {
                         generator.skipQuiets();
                         continue;
