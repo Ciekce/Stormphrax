0407aeae97c65dadd093cd1f5f03f952b81f5460
Ciekce <44617491+Ciekce@users.noreply.github.com>
2026-02-17 21:59:33 +0000

    tunability + version
    Bench: 5314852

--- src/search.cpp
+++ src/search.cpp
@@ -864,7 +864,7 @@ namespace stormphrax::search {
             MoveGenerator::main(pos, moveStack.movegenData, ttMove, curr.killers, thread.history, thread.conthist, ply);
 
         u32 legalMoves = 0;
-        u32 alphaRaises = 0;
+        i32 alphaRaises = 0;
 
         while (const auto move = generator.next()) {
             if (move == curr.excluded) {
@@ -1004,7 +1004,7 @@ namespace stormphrax::search {
                     r -= givesCheck * lmrCheckReductionScale();
                     r += cutnode * lmrCutnodeReductionScale();
                     r += (curr.ttpv && ttHit && ttEntry.score <= alpha) * lmrTtpvFailLowReductionScale();
-                    r += alphaRaises * 64;
+                    r += alphaRaises * lmrAlphaRaiseReductionScale();
 
                     if (complexity) {
                         const bool highComplexity = *complexity > lmrHighComplexityThreshold();
