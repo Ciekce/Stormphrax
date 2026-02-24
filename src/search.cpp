7a0f4de4aa67fbc424ba4bbab0bcc9074a02f8b9
Ciekce <44617491+Ciekce@users.noreply.github.com>
2026-02-24 13:42:35 +0000

    fix stalemates in datagen
    Bench: 4999557

--- src/search.cpp
+++ src/search.cpp
@@ -211,6 +211,9 @@ namespace stormphrax::search {
     std::pair<Score, Score> Searcher::runDatagenSearch() {
         auto& thread = *m_threadData[0];
 
+        // ensure a nullmove is returned in mate and stalemate
+        thread.pvMove().pv.reset();
+
         if (initRootMoveList(thread.rootPos) == RootStatus::kNoLegalMoves) {
             return {-kScoreMate, -kScoreMate};
         }
