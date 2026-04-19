6545dcc90f52deab5b846e960915222b7285dddd
Ciekce <44617491+Ciekce@users.noreply.github.com>
2026-04-19 08:09:28 +0100

    neater
    Bench: 6751620

--- src/search.cpp
+++ src/search.cpp
@@ -1306,7 +1306,7 @@ namespace stormphrax::search {
                 }
             }
         } else if (!kRootNode && parent->move && parent->quiet) {
-            const auto bonus = historyBonus(depth, 300, 100, 2500) / 2;
+            const auto bonus = historyBonus(depth, 150, 50, 1250);
             thread.history.updateMainHistory(parent->threats, parent->moving, parent->move, bonus);
         }
 
