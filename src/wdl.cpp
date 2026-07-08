aeac0cd527e77a675a651ec9a3240ec00356a1f8
Ciekce <44617491+Ciekce@users.noreply.github.com>
2026-07-08 19:02:50 +0100

    disable normalisation + sharpness
    Bench: 10296037

--- src/wdl.cpp
+++ src/wdl.cpp
@@ -36,54 +36,34 @@ namespace stormphrax::wdl {
 
         const auto m = static_cast<f64>(std::clamp(material, 17, 78)) / 58.0;
 
-        return {((kAs[0] * m + kAs[1]) * m + kAs[2]) * m + kAs[3], ((kBs[0] * m + kBs[1]) * m + kBs[2]) * m + kBs[3]};
+        return {
+            ((kAs[0] * m + kAs[1]) * m + kAs[2]) * m + kAs[3],
+            ((kBs[0] * m + kBs[1]) * m + kBs[2]) * m + kBs[3],
+        };
     }
 
     std::pair<i32, i32> wdlModel(Score povScore, i32 material) {
         const auto [a, b] = wdlParams(material);
 
-        const auto x = static_cast<f64>(povScore);
+        const auto x = static_cast<f64>(povScore) * 2.5;
 
         return {
             static_cast<i32>(std::round(1000.0 / (1.0 + std::exp((a - x) / b)))),
-            static_cast<i32>(std::round(1000.0 / (1.0 + std::exp((a + x) / b))))
+            static_cast<i32>(std::round(1000.0 / (1.0 + std::exp((a + x) / b)))),
         };
     }
 
     template <bool kSharpen>
     Score normalizeScore(Score score, i32 material) {
-        // don't normalise wins/losses, or zeroes that are pointless to normalise
-        if (score == 0 || isDecisive(score)) {
-            return score;
-        }
-
-        const auto [a, b] = wdlParams(material);
-
-        auto normalized = static_cast<f64>(score) / a;
-
-        if (kSharpen && g_opts.evalSharpness != 100) {
-            const auto power = static_cast<f64>(g_opts.evalSharpness) / 100.0;
-
-            auto sharpened = std::pow(std::abs(normalized), power);
-
-            // Damp large evals so they don't enter win range
-            if (g_opts.evalSharpness > 100) {
-                const auto clamp = (std::abs(normalized) * 300.0) / (std::abs(normalized) + 50.0);
-                sharpened = std::min(sharpened, clamp);
-            }
-
-            normalized = std::copysign(sharpened, normalized);
-        }
-
-        return static_cast<Score>(std::round(100.0 * normalized));
+        SP_UNUSED(material);
+        return score;
     }
 
     template Score normalizeScore<false>(Score, i32);
     template Score normalizeScore<true>(Score, i32);
 
     Score unnormalizeScore(Score score, i32 material) {
-        const auto [a, b] = wdlParams(material);
-        const auto unnormalized = static_cast<f64>(score) * a / 100.0;
-        return static_cast<Score>(std::round(unnormalized));
+        SP_UNUSED(material);
+        return score;
     }
 } // namespace stormphrax::wdl
